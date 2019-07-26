f9046c5e118cb158b9572c97d9d4e37e5bda4858
Mr.W <voipman@sina.cn>
2019-07-27 07:35:05 +0800

    add param

--- src/common/util/snowflakeid_test.go
+++ src/common/util/snowflakeid_test.go
@@ -6,7 +6,11 @@ import (
 
 func TestSnowFlakeWorkderId1(t *testing.T) {
 	sf := NewSnowFlakeWorkerId()
+	sf.SetDataCenterId(22)
+	sf.SetWorkderId(1)
 	id, err := sf.NextId()
+	t.Logf("id:%v, err:%v\n", id, err)
+	id, err = sf.NextId()
 	t.Logf("id:%v, err:%v\n", id, err)
 	count := 20000000
 	i := 0
